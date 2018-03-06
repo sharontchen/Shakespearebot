a75bec7c84e060a79e12e3d0a6370cfb4559cee7
Sharon Chen <stc782@gmail.com>
2018-03-06 15:45:50 -0800

    Add function to load syllable dictionary from txt file

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,8 @@
 # ----------------
 # Loads and pre-processes Shakespearean sonnet data to train on.
 
+import nltk
+
 def import_shakespeare():
     '''
     Imports dataset of Shakespearean sonnets and returns all the sonnets as a
@@ -41,3 +43,33 @@ def import_spenser():
             sonnet.append(line.strip())
     f.close()
     return sonnets
+
+def load_syllable_dict():
+    '''
+    Loads the syllable dictionary from the text file and returns it as a list of
+    lists. Each element in the list has this form:
+        [word, end, syllables]
+            word:       the word
+            end:        the number of syllables in the word if it appears at the
+                        end of the line
+            syllables:  a list of integers, representing the possible number of
+                        syllables the word can have
+    '''
+    words = []
+    f = open('data/syllable_dictionary.txt', 'r')
+    for line in f:
+        line = line.split()
+        word = []
+        word.append(line[0])
+        end = 0
+        syllables = []
+        for i in range(1, len(line)):
+            if line[i][0] == 'E':
+                end = int(line[i][1:])
+            else:
+                syllables.append(int(line[i]))
+        word.append(end)
+        word.append(syllables)
+        words.append(word)
+    f.close()
+    return words
