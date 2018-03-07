0d64711df8c364ebff8a727f1fa5a37c5ed2ee7e
Sharon Chen <stc782@gmail.com>
2018-03-07 15:09:53 -0800

    Update function to process Shakespeare sonnets

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,52 @@
 # preprocessing.py
 # Loads and pre-processes Shakespearean sonnet data to train on.
 
-def process_data(sonnets):
+def processed_shakespeare_data():
     '''
     Takes in the data in the form outputted by `load_shakespeare` or
     `load_spenser` and processes it to generate a list of sequences for each
-    sonnet. For now, ignore punctuation and treat the entire sonnet as a single
+    sonnet. Returns `data`, the sequences as a list of lists of integers, as
+    well as `dictionary`, which maps each integer to a word.
+    For now, we ignore punctuation and treat the entire sonnet as a single
     training sequence.
     '''
-    data = []
+    sonnets = load_shakespeare()
+
+    # Tokenize the sonnets into individual words.
+    X = []
     for sonnet in sonnets:
         line_new = []
         for line in sonnet:
             for word in line.split():
-                if word[-1] in [',', '.', '?', ':']:
-                    word = word[:-1]
-                if word[-1] == "'" and word[-2] not in ['t', 'h']:
-                    word = word[:-1]
-                if word[0] == "'" and word[1].isupper():
+                if word[0] == '(':
                     word = word[1:]
+                if word[0] == "'" and word[1:].lower() not in \
+                ['gainst', 'greeing,', 'scaped', 'tis', 'twixt']:
+                    word = word[1:]
+                if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
+                    word = word[:-1]
+                if word[-1] == "'" and word[:-1].lower() not in ['t', 'th']:
+                    word = word[:-1]
+                if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
+                    word = word[:-1]
                 line_new.append(word.lower())
-        data.append(line_new)
-    return data
+        X.append(line_new)
+
+    # Convert the sequences of words to sequences of integers.
+    syllable_dict = load_syllable_dict()
+    dictionary = [word[0] for word in syllable_dict]
+    for i in range(len(X)):         # loop over each sonnet
+        for j in range(len(X[i])):  # loop over every word in each sonnet
+            X[i][j] = dictionary.index(X[i][j])
+
+    # Create dictionaries for syllable information.
+    syllables = {}
+    end_syllables = {}
+    for word in syllable_dict:
+        syllables[word[0]] = word[2]
+        end_syllables[word[0]] = word[1]
+
+    return X, dictionary, syllables, end_syllables
 
 def load_shakespeare():
     '''
