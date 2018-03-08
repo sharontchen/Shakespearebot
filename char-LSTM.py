2005c103face1ab7521281b59d8a30100558cc0a
pamela-zhang <pamela.zhang@caltech.edu>
2018-03-08 02:10:56 -0800

    Output file that contains all generated sonnets for each epoch for given temperatures, batch size, epochs, step size, minimum loss, and epoch at which minimum loss occurs.

--- char-LSTM.py
+++ char-LSTM.py
@@ -40,13 +40,22 @@ for i, sentence in enumerate(sentences):
 
 # build the model: a single LSTM
 print('Build model...')
+units = 128
+epoch_num = 60
+
+
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(LSTM(units, input_shape=(maxlen, len(chars))))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
 optimizer = RMSprop(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+with open('char-LSTM-log-5.txt', 'a') as f:
+	f.write('units: ' + str(units))
+	f.write('\nstep: ' + str(step))
+	f.write('\nepochs: ' + str(epoch_num))
+
 
 
 def sample(preds, temperature=1.0):
@@ -61,38 +70,53 @@ def sample(preds, temperature=1.0):
 
 def on_epoch_end(epoch, logs):
     # Function invoked at end of each epoch. Prints generated text.
-    print()
-    print('----- Generating text after Epoch: %d' % epoch)
+    with open('char-LSTM-log-5.txt', 'a') as f:
+	    print()
+	    print('----- Generating text after Epoch: %d' % epoch)
+	    f.write('\n----- Generating text after Epoch: %d' % epoch)
 
-    start_index = random.randint(0, len(text) - maxlen - 1)
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print('----- diversity:', diversity)
+	    start_index = random.randint(0, len(text) - maxlen - 1)
+	    for diversity in [0.25, 0.75, 1.5]:
+	        print('----- diversity:', diversity)
+	        f.write('\n----- diversity:' + str(diversity))
 
-        generated = ''
-        sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(generated)
+	        generated = ''
+	        sentence = text[start_index: start_index + maxlen]
+	        if epoch == 0:
+	        	sentence = 'shall i compare thee to a summer\'s day?\n'
+	        generated += sentence
+	        print('----- Generating with seed: "' + sentence + '"')
+	        f.write('\n----- Generating with seed: "' + sentence + '"')
+	        sys.stdout.write(generated)
+	        f.write('\n' + generated)
 
-        for i in range(560):
-            x_pred = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x_pred[0, t, char_indices[char]] = 1.
+	        for i in range(560):
+	            x_pred = np.zeros((1, maxlen, len(chars)))
+	            for t, char in enumerate(sentence):
+	                x_pred[0, t, char_indices[char]] = 1.
 
-            preds = model.predict(x_pred, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
+	            preds = model.predict(x_pred, verbose=0)[0]
+	            next_index = sample(preds, diversity)
+	            next_char = indices_char[next_index]
 
-            generated += next_char
-            sentence = sentence[1:] + next_char
+	            generated += next_char
+	            sentence = sentence[1:] + next_char
 
-            sys.stdout.write(next_char)
-            sys.stdout.flush()
-        print()
+	            sys.stdout.write(next_char)
+	            f.write(next_char)
+	            sys.stdout.flush()
+	        print()
 
 print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
-model.fit(x, y,
-          batch_size=128,
-          epochs=60,
+model_history = model.fit(x, y,
+          batch_size=units,
+          epochs=epoch_num,
           callbacks=[print_callback])
+
+with open('char-LSTM-log-5.txt', 'a') as f:
+	losses = model_history.history['loss']
+	min_loss = min(losses)
+	min_iter = losses.index(min_loss)
+	f.write('\nmin loss: ' + str(min_loss))
+	f.write('\nmin loss epoch: ' + str(min_iter))
