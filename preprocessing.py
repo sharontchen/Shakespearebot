5f5a2de72d14bc279459400884ce116b854a80eb
Sharon Chen <stc782@gmail.com>
2018-03-08 16:46:39 -0800

    Add second preprocessing function to incorporate rhyme in each line

--- preprocessing.py
+++ preprocessing.py
@@ -3,8 +3,8 @@
 
 def processed_shakespeare_data():
     '''
-    Process dataset of all Shakespearean sonnets. For now, we ignore punctuation
-    and treat the entire sonnet as a single training sequence.
+    Process dataset of all Shakespearean sonnets. We ignore punctuation and
+    treat each sonnet as a single training sequence.
 
     Outputs:
         X               The input sequences. Each sequence is a sonnet, and each
@@ -57,47 +57,85 @@ def processed_shakespeare_data():
 
     return X, dictionary, syllables, end_syllables
 
-def rhyming_dict():
+def processed_shakespeare_data2():
     '''
-    Returns a list, where each element is a list of the form [word1, word2].
-    word1 and word2 rhyme, based on the Shakespearean sonnet structure.
+    Process dataset of all Shakespearean sonnets. We ignore punctuation and
+    treat each line as a single training sequence.
+
+    Outputs:
+        X               The input sequences. Each sequence is a line, and each
+                        line is represented as a list of integers, where the
+                        integer is the index of the word in `dictionary`.
+        dictionary      A list of all the words in all of Shakespeare's sonnets.
+        syllables       A Python dictionary which maps a word to a list of the
+                        number of syllables that word can have.
+        end_syllables   A Python dictionary which maps a word to a number
+                        representing the number of syllables it has when the
+                        word appears at the end of the line. If the number is 0,
+                        then the word doesn't appear at the end of a line.
+        rhyme_dict      A list of lists of the form [word1, word2], where word1
+                        and word2 are rhyming words. The words are represented
+                        as integers corresponding to the index of the word in
+                        `dictionary`.
     '''
+    # Load dictionary from text file.
+    syllable_dict = load_syllable_dict()
+    dictionary = [word[0] for word in syllable_dict]
+
+    # Create dictionaries for syllable information.
+    syllables = {}
+    end_syllables = {}
+    for word in syllable_dict:
+        syllables[word[0]] = word[2]
+        end_syllables[word[0]] = word[1]
+
+    # Load Shakespeare's sonnets.
     sonnets = load_shakespeare()
 
-    # Tokenize the sonnets into individual words.
-    X = []
+    # Tokenize the sonnets into individual words, represented as integers.
+    data = []
     for i, sonnet in enumerate(sonnets):
-        if i != 98 and i != 125:    # ignore sonnets 99 and 126
-            sonnet_new = []
-            for line in sonnet:
-                line_new = []
-                for word in line.split():
-                    if word[0] == '(':
-                        word = word[1:]
-                    if word[0] == "'" and word[1:].lower() not in \
-                    ['gainst', 'greeing,', 'scaped', 'tis', 'twixt']:
-                        word = word[1:]
-                    if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
-                        word = word[:-1]
-                    if word[-1] == "'" and word[:-1].lower() not in ['t', 'th']:
-                        word = word[:-1]
-                    if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
-                        word = word[:-1]
-                    line_new.append(word.lower())
-                sonnet_new.append(line_new)
-            X.append(sonnet_new)
+        sonnet_new = []
+        for line in sonnet:
+            line_new = []
+            for word in line.split():
+                if word[0] == '(':
+                    word = word[1:]
+                if word[0] == "'" and word[1:].lower() not in \
+                ['gainst', 'greeing,', 'scaped', 'tis', 'twixt']:
+                    word = word[1:]
+                if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
+                    word = word[:-1]
+                if word[-1] == "'" and word[:-1].lower() not in ['t', 'th']:
+                    word = word[:-1]
+                if word[-1] in [',', '.', '?', '!', ':', ';', ')']:
+                    word = word[:-1]
+                line_new.append(dictionary.index(word.lower()))
+            sonnet_new.append(line_new)
+        data.append(sonnet_new)
 
+    # Generate rhyming dictionary based on the words that appear at the end of
+    # the lines in Shakspeare's sonnets.
     rhyme_dict = []
-    for i, sonnet in enumerate(X):  # rhyme scheme is abab cdcd efef gg
-        rhyme_dict.append([sonnet[0][-1], sonnet[2][-1]])
-        rhyme_dict.append([sonnet[1][-1], sonnet[3][-1]])
-        rhyme_dict.append([sonnet[4][-1], sonnet[6][-1]])
-        rhyme_dict.append([sonnet[5][-1], sonnet[7][-1]])
-        rhyme_dict.append([sonnet[8][-1], sonnet[10][-1]])
-        rhyme_dict.append([sonnet[9][-1], sonnet[11][-1]])
-        rhyme_dict.append([sonnet[12][-1], sonnet[13][-1]])
+    for i, sonnet in enumerate(data):  # rhyme scheme is abab cdcd efef gg
+        if i != 98 and i != 125:    # ignore sonnets 99 and 126
+            rhyme_dict.append([sonnet[0][-1], sonnet[2][-1]])
+            rhyme_dict.append([sonnet[1][-1], sonnet[3][-1]])
+            rhyme_dict.append([sonnet[4][-1], sonnet[6][-1]])
+            rhyme_dict.append([sonnet[5][-1], sonnet[7][-1]])
+            rhyme_dict.append([sonnet[8][-1], sonnet[10][-1]])
+            rhyme_dict.append([sonnet[9][-1], sonnet[11][-1]])
+            rhyme_dict.append([sonnet[12][-1], sonnet[13][-1]])
 
-    return rhyme_dict
+    # Right now, `X` is a list of list of lists. The outer list contains each
+    # sonnet, the middle list contains each line in the sonnet, and the inner
+    # list contains each word in the line.
+    # Convert `X` to a list of lists, so it becomes a list of input sequences we
+    # can train on. Do this by just ignoring the sonnet and treating every line
+    # as the same.
+    X = [x for y in data for x in y]
+
+    return X, dictionary, syllables, end_syllables, rhyme_dict
 
 def load_shakespeare():
     '''
